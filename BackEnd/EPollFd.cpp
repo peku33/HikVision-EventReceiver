3b0163781fa574ffa4a9ded75284e3c3bd7a4164
Paweł Kubrak <peku33@gmail.com>
2016-05-28 17:02:25 +0200

    + EPollFd implementation

--- BackEnd/EPollFd.cpp
+++ BackEnd/EPollFd.cpp
@@ -1,1 +1,58 @@
 #include "EPollFd.hpp"
+
+#include <stdexcept>
+
+#include "EPoll.hpp"
+
+EPollFd::EPollFd(EPoll & EP, const uint32_t Events, const OnEPoll_f & OnEPoll):
+	EP(EP), Events(Events), OnEPoll(OnEPoll), Enabled(false)
+{
+
+}
+EPollFd::~EPollFd()
+{
+	if(Enabled)
+		Disable();
+}
+
+void EPollFd::Acquire(const int NewFd)
+{
+	return Fd::Acquire(NewFd);
+}
+void EPollFd::Close()
+{
+	if(Enabled)
+		Disable();
+
+	return Fd::Close();
+}
+
+void EPollFd::Enable()
+{
+	if(Enabled)
+		throw std::logic_error("Enabled (EPollFd::Enable())");
+
+	if(!IsAcquired())
+		throw std::logic_error("!IsAcquired() (EPollFd::Enable())");
+
+	EP.EPollFdRegister(this);
+	Enabled = true;
+}
+void EPollFd::Disable()
+{
+	if(!Enabled)
+		throw std::logic_error("!Enabled (EPollFd::Disable())");
+
+	if(!IsAcquired())
+		throw std::logic_error("!IsAcquired() (EPollFd::Disable())");
+
+	EP.EPollFdUnRegister(this);
+	Enabled = false;
+}
+void EPollFd::ChangeEvents(const uint32_t NewEvents)
+{
+	Events = NewEvents;
+
+	if(Enabled)
+		EP.EPollFdModify(this);
+}
--- BackEnd/EPollFd.hpp
+++ BackEnd/EPollFd.hpp
@@ -3,16 +3,38 @@
 #include <functional>
 
 #include "Fd.hpp"
-#include "EPoll.hpp"
+
+class EPoll;
 
 class EPollFd : public Fd
 {
 	public:
 		typedef std::function<void (const uint32_t)> OnEPoll_f;
 
+	public:
+		EPollFd(EPoll & EP, const uint32_t Events, const OnEPoll_f & OnEPoll);
+		~EPollFd();
+
+		//Copy, move constructors implicitly deleted because of Fd base.
+
+	private:
+		EPoll & EP;
+
 	private:
 		friend EPoll;
 		
 		uint32_t Events;
 		const OnEPoll_f OnEPoll;
+
+	private:
+		bool Enabled;
+
+	public:
+		void Acquire(const int NewFd);
+		void Close();
+
+	public:
+		void Enable();
+		void Disable();
+		void ChangeEvents(const uint32_t NewEvents);
 };
